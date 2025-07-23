88d57bcb6b5f18959bf8a10739d7af3abe2fa54d
Tales <59647566+ItzTas@users.noreply.github.com>
2025-07-23 11:49:50 -0400

    verbose flag added

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from src.constants import MODEL
+from src.constants import MODEL, VERBOSE_FLAG
 
 
 def main():
+    arg_parser = add_flags()
+    args = arg_parser.parse_args()
+
     _ = load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -16,7 +19,7 @@ def main():
         print("must provide an argument")
         exit(1)
 
-    user_prompt = sys.argv[1]
+    user_prompt = args.prompt
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -27,18 +30,31 @@ def main():
         contents=messages,
     )
 
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if is_flag_active(VERBOSE_FLAG["name"], arg_parser):
+        print_verbose(
+            user_prompt,
+            response,
+        )
+    else:
+        print_non_verbose(user_prompt, response)
+
 
 def add_flags() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
+
     _ = parser.add_argument(
-        '--verbose',
-        action='store_true',
-        help='Activates verbose logging'
+        "prompt",
+        type=str,
+        help="User prompt text",
+    )
+
+    _ = parser.add_argument(
+        f"--{VERBOSE_FLAG['name']}",
+        action=VERBOSE_FLAG["action"],
+        help=VERBOSE_FLAG["description"],
     )
     return parser
+
 
 def is_flag_active(flag: str, parser: argparse.ArgumentParser | None = None) -> bool:
     if parser is None:
@@ -46,9 +62,16 @@ def is_flag_active(flag: str, parser: argparse.ArgumentParser | None = None) -> 
     args = parser.parse_args(sys.argv[1:])
     return bool(getattr(args, flag, False))
 
-def print_non_verbose(user_prompt: str):
 
-def print_verbose():
+def print_non_verbose(user_prompt: str, response: types.GenerateContentResponse):
+    print(user_prompt)
+    print(response.text)
+
+
+def print_verbose(user_prompt: str, response: types.GenerateContentResponse):
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,18 @@
+from typing import TypedDict
+
+
+class FlagSpec(TypedDict):
+    name: str
+    type: type
+    action: str
+    description: str
+
+
+VERBOSE_FLAG: FlagSpec = {
+    "name": "verbose",
+    "type": bool,
+    "action": "store_true",
+    "description": "Activates verbose logging",
+}
+
 MODEL = "gemini-2.0-flash-001"
